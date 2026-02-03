b1fab5513962903cb9a02494393b8a160cfb393b
doanane <anane365221@gmail.com>
2026-02-03 19:35:11 +0000

    learnt about sampling and Qunatization, so implemented it as code to help me understand it well

---
+++ Sampling&Quantization.py
@@ -0,0 +1,26 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread("images/dog.png")
+
+img_rgb= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+img_gray =cv2.imread("images/dog.png", cv2.IMREAD_GRAYSCALE)
+
+print("data type", img.dtype)
+print("Image shape:", img.shape)
+print("min pixel:", img.min())
+print("max pixel:", img.max())
+
+plt.figure(figsize=(10,4))
+plt.subplot(1,2,1)
+plt.title("Original Image")
+plt.axis("off")
+plt.imshow(img_rgb)
+
+
+plt.subplot(1,2,2)
+plt.title("Original Grayscale Image")
+plt.axis("off")
+plt.imshow(img_gray, cmap='gray')
+plt.show()
